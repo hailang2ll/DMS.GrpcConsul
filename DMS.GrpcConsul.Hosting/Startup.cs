5cd848d79647cecfba5472586a689d8fb2f7051f
hailang2ll <79522860@qq.com>
2018-08-27 18:13:49 +0800

    修改启动配置

--- DMS.GrpcConsul.Hosting/Startup.cs
+++ DMS.GrpcConsul.Hosting/Startup.cs
@@ -26,7 +26,7 @@ namespace DMS.GrpcConsul.Hosting
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             //注册全局配置
